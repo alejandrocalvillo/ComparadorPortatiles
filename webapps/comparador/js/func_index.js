6fcc0c453f80047376257c6a67897f5b3081c131
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-05-09 10:56:14 +0200

    Error gravisimo

--- webapps/comparador/js/func_index.js
+++ webapps/comparador/js/func_index.js
@@ -62,7 +62,6 @@ function seleccionarOrdenador(index) {
 		indicesSeleccionados.push(index);
 
 		const ordenadorSeleccionado = document.createElement('div');
-		ordenadorSeleccionado.
 		ordenadorSeleccionado.innerHTML = `<p>Marca: ${ordenador.marca}</p> <p>Modelo: ${ordenador.modelo}</p>`;
 		ordenadorSeleccionado.setAttribute('class', `md-4 mb-3`);
 		const btnEliminar = document.createElement('input');
