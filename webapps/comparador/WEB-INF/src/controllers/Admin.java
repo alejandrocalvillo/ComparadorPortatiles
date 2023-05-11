4435c87bcb789088c3c9462e88de7ca40c2a5fa7
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-05-11 18:18:10 +0200

    Al admin

--- webapps/comparador/WEB-INF/src/controllers/Admin.java
+++ webapps/comparador/WEB-INF/src/controllers/Admin.java
@@ -27,16 +27,21 @@ public class Admin extends HttpServlet {
     public void doGet(HttpServletRequest request, HttpServletResponse response) throws IOException, ServletException {
         HttpSession session = request.getSession();
         Usuario usuario = (Usuario) session.getAttribute("usuario");
-        if (usuario != null && usuario.getNombre().equals("admin")
-                && usuario.getContrasena().equals("*00A51F3F48415C7D4E8908980D443C29C69B60C9")) {
-            RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/index_admin.jsp");
-            rd.forward(request, response);
-        } else {
-            response.sendRedirect(request.getContextPath() + "/index");
-        }
+        // if (usuario != null && usuario.getNombre().equals("admin")
+        //         && usuario.getContrasena().equals("*00A51F3F48415C7D4E8908980D443C29C69B60C9")) {
+        //     RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/index_admin.jsp");
+        //     rd.forward(request, response);
+        // } else {
+        //     response.sendRedirect(request.getContextPath() + "/index");
+        // }
 
         try (DBManager db = new DBManager()) {
-            // Empezamos contando marcas en el index
+            if(db.isAdmin(String.valueOf(usuario.getId()))) {
+                RequestDispatcher rd = request.getRequestDispatcher("WEB-INF/jsp/index_admin.jsp");
+                rd.forward(request, response);
+            } else {
+                response.sendRedirect(request.getContextPath() + "/index");
+            }
 
         } catch (SQLException | NamingException e) {
             e.printStackTrace();
