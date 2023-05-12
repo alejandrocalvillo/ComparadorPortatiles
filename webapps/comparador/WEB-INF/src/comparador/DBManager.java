96948918d8978df64b39c1b02f5db077bf92e5c3
Alejandro Calvillo <alejandrocalvillo.it@gmail.com>
2023-05-12 02:03:43 +0200

    Ahora?

--- webapps/comparador/WEB-INF/src/comparador/DBManager.java
+++ webapps/comparador/WEB-INF/src/comparador/DBManager.java
@@ -46,15 +46,15 @@ public class DBManager implements AutoCloseable {
      */
     public List<Ordenador> getOrdenadores(int start, int limit) {
         List<Ordenador> ordenadores = new ArrayList<>();
-        String query = "SELECT ordenadores.id AS id, ordenadores.modelo, marcas.nombre AS marca_nombre, procesadores.nombre AS procesador_nombre, memorias.tipo AS memoria_tipo, memorias.capacidad AS capacidad_ram, discos.tipo AS disco_tipo, discos.capacidad AS capacidad_disco, puntos_de_venta.tienda, puntos_de_venta.precio FROM ordenadores INNER JOIN marcas ON marca_id = marcas.id INNER JOIN procesadores ON procesador_id = procesadores.id INNER JOIN discos ON disco_id = discos.id INNER JOIN memorias ON memoria_id = memorias.id INNER JOIN puntos_de_venta ON ordenadores.id = puntos_de_venta.ordenador_id ORDER BY id DESC LIMIT "+start+", "+limit;
+        String query = "SELECT ordenadores.id AS id, ordenadores.modelo, marcas.nombre AS marca_nombre, procesadores.nombre AS procesador_nombre, memorias.tipo AS memoria_tipo, memorias.capacidad AS capacidad_ram, discos.tipo AS disco_tipo, discos.capacidad AS capacidad_disco, puntos_de_venta.tienda, puntos_de_venta.precio FROM ordenadores INNER JOIN marcas ON marca_id = marcas.id INNER JOIN procesadores ON procesador_id = procesadores.id INNER JOIN discos ON disco_id = discos.id INNER JOIN memorias ON memoria_id = memorias.id INNER JOIN puntos_de_venta ON ordenadores.id = puntos_de_venta.ordenador_id ORDER BY id DESC LIMIT "+start+", "+limit+"";
         try (Statement stmt = connection.createStatement()) {
             ResultSet resultSet = stmt.executeQuery(query);
             while (resultSet.next()) {
                 Ordenador ordenador = new Ordenador();
                 ordenador.setId(resultSet.getInt("id"));
-                ordenador.setMarca(resultSet.getString("marca"));
+                ordenador.setMarca(resultSet.getString("marca_nombre"));
                 ordenador.setModelo(resultSet.getString("modelo"));
-                ordenador.setProcesador(resultSet.getString("procesador"));
+                ordenador.setProcesador(resultSet.getString("procesador_nombre"));
                 ordenador.setMemoriaTipo(resultSet.getString("memoria_tipo"));
                 ordenador.setMemoriaCapacidad(resultSet.getInt("capacidad_ram"));
                 ordenador.setDiscoTipo(resultSet.getString("disco_tipo"));
